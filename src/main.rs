0ab6b700f1505863ba50bdf61a6a2c09e09b793d
remique <emj1054@gmail.com>
2023-10-18 19:13:39 +0200

    Basic append to LOG file

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,95 @@
-use std::collections::BTreeMap;
+use glob::glob;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
+use std::{collections::BTreeMap, fs::OpenOptions};
 
 use clap::{Arg, Command};
 
+type FileId = i32;
+
+const KIVI_DIR: &str = "db";
+const DATA_DIR: &str = "data";
+const HINTS_DIR: &str = "hints";
+
 struct KiviStore {
     mem_index: BTreeMap<String, String>,
+    active_file: File,
+    // stale_files: Vec<FileId>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Record {
+    key: String,
+    value: String,
+}
+
+struct InternalRecord {
+    file_id: String,
+    value_size: i32,
+    value_pos: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+enum KiviCommand {
+    Set { key: String, value: String },
+    Delete { key: String },
 }
 
 impl KiviStore {
     fn new() -> Self {
-        let mem_index = BTreeMap::new();
+        let mut mem_index = BTreeMap::new();
 
-        Self { mem_index }
+        let active_file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .read(true)
+            .open("1.log")
+            .unwrap();
+
+        Self {
+            mem_index,
+            active_file,
+        }
     }
 
     fn get(&self, key: String) {
-        println!("Doing KiviStore GET. Key: {}", key);
+        // println!("Doing KiviStore GET. Key: {}", key);
+        let get_by_key = self.mem_index.get(&key);
+        if let Some(val) = get_by_key {
+            println!("KiviStore GET: Got {}", val);
+        } else {
+            println!("KiviStore GET: not found");
+        }
     }
 
-    fn set(&self, key: String, value: String) {
+    fn set(&mut self, key: String, value: String) {
         println!("Doing KiviStore SET. Key: {}, Value: {}", key, value);
+
+        let set_com = KiviCommand::Set { key, value };
+
+        let j = serde_json::to_string(&set_com).unwrap();
+
+        self.active_file.write_all(j.as_bytes()).unwrap();
+        // self.mem_index.insert(key, value);
+    }
+
+    fn build_keydir(&mut self) {
+        // first we iterate over all stale files decreasing order
+        // and then insert keys and values
+    }
+}
+
+fn get_files() {
+    let xd = glob(format!("./{}/{}/*.data", KIVI_DIR, DATA_DIR).as_ref()).unwrap();
+
+    for item in xd {
+        println!("{:?}", item);
     }
 }
 
 fn main() {
-    let ks = KiviStore::new();
+    let mut ks = KiviStore::new();
 
     let m = Command::new("kivi")
         .subcommand(
