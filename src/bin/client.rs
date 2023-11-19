577f20bbb967e0189908fc7f84cc9cf7a1f3866e
remique <emj1054@gmail.com>
2023-11-19 17:58:40 +0100

    More work on client & server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,21 +1,23 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::str;
 
 fn main() {
     let conn = TcpStream::connect("127.0.0.1:7878");
+    let args = std::env::args().collect::<Vec<String>>();
+
+    // Skip first argument
+    let s = args[1..].join(" ");
 
     match conn {
         Ok(mut stream) => {
             println!("Connected to server");
-            loop {
-                let mut s = String::new();
-                std::io::stdin().read_line(&mut s).unwrap();
 
-                if s.is_empty() {
-                    break;
-                }
-                stream.write_all(s.as_bytes()).unwrap();
-            }
+            stream.write_all(s.as_bytes()).unwrap();
+
+            let mut buf = Vec::new();
+            stream.read_to_end(&mut buf).expect("failed to read to end");
+            println!("Response: {:?}", str::from_utf8(&buf).unwrap());
         }
         Err(e) => {
             println!("Could not connect: {}", e);
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,25 +1,87 @@
-use std::io::Read;
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 use std::str;
+use std::thread;
+
+fn initialize_logger() {
+    let env = env_logger::Env::default()
+        .filter_or("MY_LOG_LEVEL", "trace")
+        .write_style_or("MY_LOG_STYLE", "always");
+
+    env_logger::init_from_env(env);
+}
+
+enum Command {
+    Set,
+    Get,
+    Invalid,
+}
+
+impl Command {
+    fn which_command(input: &str) -> Command {
+        match input.as_bytes() {
+            b"set" => Command::Set,
+            b"get" => Command::Get,
+            _ => Command::Invalid,
+        }
+    }
+}
+
+// This is probably not the best function, but should work
+fn stream_to_vec(buf: &[u8]) -> Vec<String> {
+    let s = str::from_utf8(buf).expect("Could not from utf8");
+
+    let v = s.split(" ").collect::<Vec<&str>>();
+
+    v.iter().map(|x| x.to_string()).collect::<Vec<String>>()
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    let read_bytes = stream.read(&mut buf).unwrap();
+
+    let vec = stream_to_vec(&buf[0..read_bytes]);
+    println!("As vec: {:?}", vec);
+    let command = Command::which_command(vec[0].as_str());
+
+    match command {
+        Command::Get => {
+            println!("Get command with arg: {}", vec[1].as_str());
+        }
+        Command::Set => {
+            println!(
+                "Set command with key: {}, value: {}",
+                vec[1].as_str(),
+                vec[2].as_str()
+            );
+        }
+        Command::Invalid => {
+            println!("Unrecognized command");
+        }
+    }
+
+    let s = str::from_utf8(&buf[0..read_bytes]).unwrap();
+    println!("{:?}", s);
+
+    // Respond
+    stream
+        .write_all("Received: {}, response: OK".as_bytes())
+        .expect("Could not respond");
+}
 
 fn main() {
+    initialize_logger();
+
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
-    println!("Server listening on port 7878");
+    log::debug!("Server listening on port 7878");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
-
-                loop {
-                    let mut buf = [0; 128];
-                    let read_bytes = stream.read(&mut buf).unwrap();
-                    println!("received bytes {}", read_bytes);
-
-                    let s = str::from_utf8(&buf[0..read_bytes]).unwrap();
-                    println!("{:?}", s);
-                }
+                thread::spawn(move || handle_client(stream));
             }
             Err(e) => {
                 println!("Error: {}", e);
